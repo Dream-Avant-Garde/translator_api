57401da259561edcb0bf1c629877901993438f77
Federico Cacace <cacacefederico05@gmail.com>
2024-03-11 20:14:08 -0300

    Add speech-to-speech translation endpoint to ws_translator.py

--- routers/ws_translator.py
+++ routers/ws_translator.py
@@ -1,5 +1,26 @@
 from fastapi import APIRouter, Request, File, UploadFile
+from fastapi.responses import StreamingResponse
+import librosa
+import io
+# local
+from src.func import return_streaming_audio
+# from src.model.seamlees_m4t import *
 
 router = APIRouter(
     prefix='/ws_translate'
 )
+
+
+@router.get('/', tags=['ws_translate'])
+async def ws_traslate():
+    return 'test'
+
+@router.post('/S2ST', tags=['ws_translate'])
+async def speech_to_speech_translation(request: Request, audio_file: UploadFile = File(...)):
+    byte_data = await audio_file.read()
+
+    b_data = io.BytesIO(byte_data.read())
+    data, sampling_rate = librosa.load(b_data)
+
+
+    return StreamingResponse(return_streaming_audio(byte_data), media_type='audio/wav')
