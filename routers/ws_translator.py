bab9b83167a5f7aacab47159ffaa17139572d619
Federico Cacace <cacacefederico05@gmail.com>
2024-03-11 21:04:31 -0300

    Update .gitignore and fix audio file reading in ws_translator.py

--- routers/ws_translator.py
+++ routers/ws_translator.py
@@ -19,8 +19,7 @@ async def ws_translate():
 async def speech_to_speech_translation(request: Request, audio_file: UploadFile = File(...)):
     byte_data = await audio_file.read()
 
-    b_data = io.BytesIO(byte_data.read())
+    b_data = io.BytesIO(byte_data)  # No necesitas llamar a .read() de nuevo
     data, sampling_rate = librosa.load(b_data)
-
 
     return StreamingResponse(return_streaming_audio(byte_data), media_type='audio/wav')
