5f4121c0c8c7949d823550a18c9e00bae9d73e29
Alan Delgado <alanhd1302@gmail.com>
2024-05-20 20:50:49 -0500

    test bot

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ async def websocket_endpoint(websocket: WebSocket):
             b_data = io.BytesIO()
             torchaudio.save(b_data, output[1].audio_wavs[0][0].to(torch.float32).cpu(), sampling_rate, format='wav')
             print('sample rate: ', sampling_rate)
-            print('len data: ', len(b_data))
+            print('len data: ', b_data)
 
             if os.path.exists('audios/'):
                 with open(f'audios/out{i-1}.wav', 'wb') as file:
